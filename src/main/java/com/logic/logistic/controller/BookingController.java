74e3b55fb9067fead72ca8e09cc6f8af5444978d
Tanuj Pandey <tanujpandey200@gmail.com>
2025-03-23 12:31:17 +0530

    Implmented Profile

--- src/main/java/com/logic/logistic/controller/BookingController.java
+++ src/main/java/com/logic/logistic/controller/BookingController.java
@@ -1,14 +1,17 @@
 package com.logic.logistic.controller;
 
+import java.util.Date;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.logic.logistic.model.Booking;
@@ -19,13 +22,15 @@ import com.logic.logistic.repository.BookingRepository;
 @RestController
 @RequestMapping("/api/bookings")
 public class BookingController {
-    @Autowired private BookingRepository bookingRepository;
-    
-    @Autowired private BookRepository bookRepository;
-    
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
     @PostMapping("/bookLoad")
     public ResponseEntity<?> bookLoad(@RequestBody Booking booking) {
-       
+        booking.setBookingDate(new Date()); // Set the current date
         return ResponseEntity.ok(bookRepository.save(booking));
     }
 
@@ -39,5 +44,12 @@ public class BookingController {
     @GetMapping("/track/{trackingId}")
     public ResponseEntity<?> trackBooking(@PathVariable String trackingId) {
         return ResponseEntity.ok(bookingRepository.findById(trackingId));
+    }
+
+    @GetMapping("/report")
+    public ResponseEntity<?> getBookingReport(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date fromDate,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date toDate) {
+        return ResponseEntity.ok(bookRepository.findByBookingDateBetween(fromDate, toDate));
     }
 }
--- src/main/java/com/logic/logistic/model/Booking.java
+++ src/main/java/com/logic/logistic/model/Booking.java
@@ -3,88 +3,124 @@ package com.logic.logistic.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
 
 @Entity
 public class Booking {
 
-	@Id
-	@GeneratedValue
-	private Long id;
-	private String consignorName;
-	private String consignorMobile;
-	private String consigneeName;
-	private String consigneeMobile;
-	private String articleType;
-	private int articleWeight;
-	private double freight;
-	private double sgst;
-	private double cgst;
-	private double igst;
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getConsignorName() {
-		return consignorName;
-	}
-	public void setConsignorName(String consignorName) {
-		this.consignorName = consignorName;
-	}
-	public String getConsignorMobile() {
-		return consignorMobile;
-	}
-	public void setConsignorMobile(String consignorMobile) {
-		this.consignorMobile = consignorMobile;
-	}
-	public String getConsigneeName() {
-		return consigneeName;
-	}
-	public void setConsigneeName(String consigneeName) {
-		this.consigneeName = consigneeName;
-	}
-	public String getConsigneeMobile() {
-		return consigneeMobile;
-	}
-	public void setConsigneeMobile(String consigneeMobile) {
-		this.consigneeMobile = consigneeMobile;
-	}
-	public String getArticleType() {
-		return articleType;
-	}
-	public void setArticleType(String articleType) {
-		this.articleType = articleType;
-	}
-	public int getArticleWeight() {
-		return articleWeight;
-	}
-	public void setArticleWeight(int articleWeight) {
-		this.articleWeight = articleWeight;
-	}
-	public double getFreight() {
-		return freight;
-	}
-	public void setFreight(double freight) {
-		this.freight = freight;
-	}
-	public double getSgst() {
-		return sgst;
-	}
-	public void setSgst(double sgst) {
-		this.sgst = sgst;
-	}
-	public double getCgst() {
-		return cgst;
-	}
-	public void setCgst(double cgst) {
-		this.cgst = cgst;
-	}
-	public double getIgst() {
-		return igst;
-	}
-	public void setIgst(double igst) {
-		this.igst = igst;
-	}
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String consignorName;
+    private String consignorMobile;
+    private String consigneeName;
+    private String consigneeMobile;
+    private String articleType;
+    private int articleWeight;
+    private double freight;
+    private double sgst;
+    private double cgst;
+    private double igst;
+    @Temporal(TemporalType.DATE)
+    private Date bookingDate; // Added booking date
 
+    // Getters and setters...
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getConsignorName() {
+        return consignorName;
+    }
+
+    public void setConsignorName(String consignorName) {
+        this.consignorName = consignorName;
+    }
+
+    public String getConsignorMobile() {
+        return consignorMobile;
+    }
+
+    public void setConsignorMobile(String consignorMobile) {
+        this.consignorMobile = consignorMobile;
+    }
+
+    public String getConsigneeName() {
+        return consigneeName;
+    }
+
+    public void setConsigneeName(String consigneeName) {
+        this.consigneeName = consigneeName;
+    }
+
+    public String getConsigneeMobile() {
+        return consigneeMobile;
+    }
+
+    public void setConsigneeMobile(String consigneeMobile) {
+        this.consigneeMobile = consigneeMobile;
+    }
+
+    public String getArticleType() {
+        return articleType;
+    }
+
+    public void setArticleType(String articleType) {
+        this.articleType = articleType;
+    }
+
+    public int getArticleWeight() {
+        return articleWeight;
+    }
+
+    public void setArticleWeight(int articleWeight) {
+        this.articleWeight = articleWeight;
+    }
+
+    public double getFreight() {
+        return freight;
+    }
+
+    public void setFreight(double freight) {
+        this.freight = freight;
+    }
+
+    public double getSgst() {
+        return sgst;
+    }
+
+    public void setSgst(double sgst) {
+        this.sgst = sgst;
+    }
+
+    public double getCgst() {
+        return cgst;
+    }
+
+    public void setCgst(double cgst) {
+        this.cgst = cgst;
+    }
+
+    public double getIgst() {
+        return igst;
+    }
+
+    public void setIgst(double igst) {
+        this.igst = igst;
+    }
+
+    public Date getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(Date bookingDate) {
+        this.bookingDate = bookingDate;
+    }
 }
