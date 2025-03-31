60e127263b0ad18301d86158cc551f8333929be8
Tanuj Pandey <tanujpandey200@gmail.com>
2025-03-31 11:07:33 +0530

    Implemented Change Password Functionality

--- src/main/java/com/logic/logistic/repository/UserRepository.java
+++ src/main/java/com/logic/logistic/repository/UserRepository.java
@@ -3,15 +3,21 @@ package com.logic.logistic.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.logic.logistic.dto.UserDto;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserDto, String>{
-	
-	
+public interface UserRepository extends JpaRepository<UserDto, String> {
+    
+    @Query(value = "SELECT * FROM logistics_logic.user_data WHERE (user_name = :username AND password = :password)", nativeQuery = true)
+    UserDto findByUsername(String username, String password);
 
-	@Query(value = "select * from logistics_logic.user_data where (user_name= :username and password =:password)", nativeQuery = true)
-	UserDto findByUsername(String username, String password);
-
+    // ✅ Added update query for changing password (without removing anything)
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE logistics_logic.user_data SET password = :newPassword WHERE user_name = :username", nativeQuery = true)
+    int updatePassword(@Param("username") String username, @Param("newPassword") String newPassword);
 }
