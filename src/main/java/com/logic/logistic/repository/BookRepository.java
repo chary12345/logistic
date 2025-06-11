758cc9433e17ae3450b205c81c185e9c25e6571f
upchary <prashanthchary2020@gmail.com>
2025-06-11 23:47:07 +0530

    get booking report data added

--- src/main/java/com/logic/logistic/repository/BookRepository.java
+++ src/main/java/com/logic/logistic/repository/BookRepository.java
@@ -1,15 +1,16 @@
 package com.logic.logistic.repository;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.logic.logistic.dto.Booking;
 
-import java.util.Date;
-import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Booking, String> {
-
-    List<Booking> findByBookingDateBetween(Date fromDate, Date toDate);
+	Page<Booking> findByBookingDateBetween(LocalDateTime fromDate, LocalDateTime toDate, Pageable pageable);
 }
