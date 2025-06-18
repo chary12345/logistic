a28ee6617fd6c132c4ec0de08cb5f58690ea250b
upchary <prashanthchary2020@gmail.com>
2025-06-18 23:05:40 +0530

    remove unnessacary code

--- src/main/java/com/logic/logistic/controller/BookingController.java
+++ src/main/java/com/logic/logistic/controller/BookingController.java
@@ -15,60 +15,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.logic.logistic.dto.Booking;
 import com.logic.logistic.model.BookingDTO;
-import com.logic.logistic.repository.BookRepository;
 import com.logic.logistic.service.BookingService;
 
 @RestController
 @RequestMapping("/api/bookings")
 public class BookingController {
-    
 
-    @Autowired
-    private BookRepository bookRepository;
-    
-    @Autowired
-    private BookingService bookingService;
+	@Autowired
+	private BookingService bookingService;
 
-    @PostMapping("/bookLoad")
-    public ResponseEntity<Booking> createBooking(@RequestBody BookingDTO dto) {
-        Booking saved = bookingService.saveBooking(dto);
-        return ResponseEntity.ok(saved);
-    }
+	@PostMapping("/bookLoad")
+	public ResponseEntity<Booking> createBooking(@RequestBody BookingDTO dto) {
+		Booking saved = bookingService.saveBooking(dto);
+		return ResponseEntity.ok(saved);
+	}
 
-	/*
-	 * @PostMapping("/bookLoad") public ResponseEntity<?> bookLoad(@RequestBody
-	 * Booking booking) { LoadRecieptPojo LrPojo=new LoadRecieptPojo();
-	 * LrPojo.setCompanyCode("UNIQ"); LrPojo.setBranchCode("DIUNIQ");
-	 * LrPojo.setLastNumber(1); String generateReceiptNumber =
-	 * GenerateUtils.generateReceiptNumber(LrPojo);
-	 * booking.setLoadingReciept(generateReceiptNumber);
-	 * booking.setBookingDate(LocalDateTime.now()); // Set the current date return
-	 * ResponseEntity.ok(bookRepository.save(booking)); }
-	 */
-	/*
-	 * @PostMapping("/fetchLoadReciept") public ResponseEntity<?>
-	 * getLoadReciet(@RequestBody LoadRecieptPojo LrPojo) {
-	 * 
-	 * String generateReceiptNumber = GenerateUtils.generateReceiptNumber(LrPojo);
-	 * return ResponseEntity.ok(generateReceiptNumber); }
-	 */
+	@GetMapping("/report")
+	public ResponseEntity<?> getBookingReport(
+			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime fromDate,
+			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime toDate,
+			@RequestParam(required = false) String status) {
+		return ResponseEntity.ok(bookingService.getBookingReportsBetweenDates(fromDate, toDate, status));
+	}
 
-    @GetMapping("/report")
-    public ResponseEntity<?> getBookingReport(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime fromDate,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime toDate,  @RequestParam(required = false) String status) {
-        return ResponseEntity.ok(bookingService.getBookingReportsBetweenDates(fromDate, toDate,status));
-    }
-    
-    @GetMapping("/todayReports")
-    public ResponseEntity<?> toDayBookingRepoprts(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime fromDate,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime toDate,String status) {
-        return ResponseEntity.ok(bookingService.getBookingReportsBetweenDates(fromDate, toDate,status));
-    }
-    @PostMapping("/dispatchLoad")
-    public ResponseEntity<List<Booking>> dispatchLoad(@RequestBody List<String> loadingReceipts) {
-       List<Booking> dispatchLoad = bookingService.dispatchLoad(loadingReceipts);
-        return ResponseEntity.ok(dispatchLoad);
-    }
+	@GetMapping("/todayReports")
+	public ResponseEntity<?> toDayBookingRepoprts(
+			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime fromDate,
+			@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime toDate, String status) {
+		return ResponseEntity.ok(bookingService.getBookingReportsBetweenDates(fromDate, toDate, status));
+	}
+
+	@PostMapping("/dispatchLoad")
+	public ResponseEntity<List<Booking>> dispatchLoad(@RequestBody List<String> loadingReceipts) {
+		List<Booking> dispatchLoad = bookingService.dispatchLoad(loadingReceipts);
+		return ResponseEntity.ok(dispatchLoad);
+	}
 }
