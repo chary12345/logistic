169ed2b1ca205d27ffc8a49b786a63232a2460c9
sridh <sridh@Sridher>
2025-06-19 22:41:08 +0530

    Loggers implemented

--- src/main/java/com/logic/logistic/LogisticApplication.java
+++ src/main/java/com/logic/logistic/LogisticApplication.java
@@ -1,13 +1,22 @@
 package com.logic.logistic;
 
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogisticApplication {
 
+//	private static final long serialVersionUID=1L;
+//	private static final Logger logger = LoggerFactory.getLogger(LogisticApplication.class);
+	
+	private static org.apache.logging.log4j.Logger logger = LogManager.getLogger();	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LogisticApplication.class, args);
+		
 	}
 
 }
--- src/main/java/com/logic/logistic/service/CompanyRegisterServiceImpl.java
+++ src/main/java/com/logic/logistic/service/CompanyRegisterServiceImpl.java
@@ -1,7 +1,9 @@
 package com.logic.logistic.service;
 
+import java.lang.System.Logger;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +22,9 @@ import com.logic.logistic.repository.CompanyRegisterrepo;
 @Service
 public class CompanyRegisterServiceImpl implements CompanyRegisterService {
 
+	private static final long serialVersionUID=1L;
 
+	private static org.apache.logging.log4j.Logger logger = LogManager.getLogger();
 
 	@Autowired
 	CompanyRegisterrepo companyRegisterrepo;
@@ -40,10 +44,12 @@ public class CompanyRegisterServiceImpl implements CompanyRegisterService {
 			CompanyDto byId = companyRegisterrepo.getCompanyByID(companyCode);
 			if (byId == null) {
 				status = "SUCCESS";
+				logger.info("SUCCESS: "+byId);
 
 			}
 		} catch (Exception e) {
 			status = e.getMessage();
+			logger.error("Exception in companycode : "+e);
 		}
 		return status;
 	}
@@ -55,10 +61,12 @@ public class CompanyRegisterServiceImpl implements CompanyRegisterService {
 			BranchDTO branchBybranchCode = branchRepo.getBranchBybranchCode(branchCode);
 			if (branchBybranchCode == null) {
 				status = "SUCCESS";
+				logger.info("SUCCESS: "+branchBybranchCode);
 
 			}
 		} catch (Exception e) {
 			status = e.getMessage();
+			logger.error("Exception in branchcode : "+e);
 		}
 		return status;
 	}
@@ -74,6 +82,7 @@ public class CompanyRegisterServiceImpl implements CompanyRegisterService {
 
 				branchRepo.save(branchDto);
 				System.out.println("branch created successfully");
+				logger.info("branch created successfully");
 
 				if (branchDto.getBranchCode() != null && branchData.getBranchAddress() != null) {
 					AddressDto addressDto = BranchAndAddressMapper.addressToAddressDto(branchData.getBranchAddress(),
@@ -81,6 +90,7 @@ public class CompanyRegisterServiceImpl implements CompanyRegisterService {
 
 					addressRepo.save(addressDto);
 					System.out.println("address created successfully");
+					logger.info("address created successfully");
 					status = "SUCCESS";
 				}
 			}
@@ -89,6 +99,7 @@ public class CompanyRegisterServiceImpl implements CompanyRegisterService {
 
 		Exception e) {
 			status = e.getMessage();
+			logger.error("Exception in branchdata"+e);
 		}
 
 		return status;
@@ -99,8 +110,9 @@ public class CompanyRegisterServiceImpl implements CompanyRegisterService {
 		List<BranchMap> branchList=null;
 		try {
 		 branchList=branchRepo.getbranchesListByCompanyCode(companyCode);
+		 logger.info("getBranchesByCompanyCode : "+branchList);
 		}catch (Exception e) {
-			
+			logger.error("Exception in getBranchesByCompanyCode : "+e);
 		}
 		return branchList;
 	}
